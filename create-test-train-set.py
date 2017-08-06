374962c6398cab6e182f93e6225c5befd3609035
Jean-Frederic Plante <jfrederic.plante@gmail.com>
2017-08-06 00:04:00 -0700

    adds copy training and testing func, dir creation, and cat 1 and 2 copying. fails on  empty list

--- create-test-train-set.py
+++ create-test-train-set.py
@@ -1,86 +1,58 @@
 import os
 import glob
-import random
-import numpy as np
+from random import shuffle
+import shutil
+from math import floor
 
-import cv2
 
-# Preprocessing images
+# Parameters
 TRAINING_PERCENTAGE = 80
-
-train_folder = '/train/'
-test_folder = '/test/'
-category_1 = 'one'
-category_2 = 'ten'
-
-os.system('rm -rf  ' + train_lmdb)
-os.system('rm -rf  ' + validation_lmdb)
-
-ones_files = [img for img in glob.glob("../input/one/*.JPG")]
-tens_files = [img for img in glob.glob("../input/ten/*.JPG")]
-
-# Selecting test and training data
-trainingsize_ones = TRAINING_PERCENTAGE * len(ones_files)
-trainingsize_tens = TRAINING_PERCENTAGE * len(tens_files)
-random.shuffle(ones_files)
-random.shuffle(tens_files)
-
-# Training and testing arrays
-train_data = ones_files[:trainingsize_ones]+tens_files[:trainingsize_tens]
-test_data = ones_files[trainingsize_ones+1:]+tens_files[trainingsize_tens+1:]
-random.shuffle(train_data)
-random.shuffle(test_data)
-
-print '\n',len(ones_files)," number of ones images and ",len(tens_files), "number of tens images"
-print 'concatenated training data ', len(train_data)
-print 'concatenated test data ', len(test_data)
-#for in_idx, img_path in enumerate(train_data):
-#	if 'one' in img_path:
-#		print 'this is a one file'
-#	else:
-#		print "this is a ten file"
-
-#train_data = [img for img in glob.glob("../input/train/*jpg")]
-#test_data = [img for img in glob.glob("../input/test1/*jpg")]
-#
-##Shuffle train_data
-#random.shuffle(train_data)
-#
-print 'Creating train_lmdb'
-
-in_db = lmdb.open(train_lmdb, map_size=int(1e12))
-with in_db.begin(write=True) as in_txn:
-    for in_idx, img_path in enumerate(train_data):
-        if in_idx %  15 == 0: # don't have that much data
-            continue
-        img = cv2.imread(img_path, cv2.IMREAD_COLOR)
-        img = transform_img(img, img_width=IMAGE_WIDTH, img_height=IMAGE_HEIGHT)
-        if 'one' in img_path:
-            label = 1
-        else:
-            label = 10
-        datum = make_datum(img, label)
-        in_txn.put('{:0>5d}'.format(in_idx), datum.SerializeToString())
-        print '{:0>5d}'.format(in_idx) + ':' + img_path
-in_db.close()
+ORIGINAL_FOLDER = "processed"
+CATEGORY_1 = "/one"
+CATEGORY_2 = "/ten"
+TRAIN_DESTINATION_FOLDER = "training"
+TEST_DESTINATION_FOLDER = "test"
 
 
-print '\nCreating validation_lmdb'
+def get_file_list_from_dir(datadir):
+    all_files = os.listdir(os.path.abspath(datadir))
+    image_files = list(filter(lambda file: file.endswith('.JPG'), all_files))
+    return image_files
 
-in_db = lmdb.open(validation_lmdb, map_size=int(1e12))
-with in_db.begin(write=True) as in_txn:
-    for in_idx, img_path in enumerate(train_data):
-        if in_idx % 6 != 0:
-            continue
-        img = cv2.imread(img_path, cv2.IMREAD_COLOR)
-        img = transform_img(img, img_width=IMAGE_WIDTH, img_height=IMAGE_HEIGHT)
-        if 'one' in img_path:
-            label = 1
-        else:
-            label = 10
-        datum = make_datum(img, label)
-        in_txn.put('{:0>5d}'.format(in_idx), datum.SerializeToString())
-        print '{:0>5d}'.format(in_idx) + ':' + img_path
-in_db.close()
 
-print '\nFinished processing all images'
+def get_training_and_testing_sets(file_list):
+    split = TRAINING_PERCENTAGE/100
+    split_index = floor(len(file_list) * split)
+    training = file_list[:split_index]
+    testing = file_list[split_index:]
+    return training, testing
+
+def copy_training_and_testing_sets(image_folder, train_folder, test_folder):
+    image_files = get_file_list_from_dir(image_folder)
+    print image_folder, image_files
+    training_files, testing_files = get_training_and_testing_sets(image_files)
+    for original_file in training_files:
+        shutil.copy(original_file,train_folder)
+    for original_file in testing_files:
+        shutil.copy(original_file, test_folder)
+
+
+print "\n creating destination folders"
+shutil.rmtree(TRAIN_DESTINATION_FOLDER, ignore_errors = True)
+shutil.rmtree(TEST_DESTINATION_FOLDER, ignore_errors = True)
+
+os.makedirs(TRAIN_DESTINATION_FOLDER+CATEGORY_1)
+os.makedirs(TRAIN_DESTINATION_FOLDER+CATEGORY_2)
+os.makedirs(TEST_DESTINATION_FOLDER)
+
+print "\n Getting training and test for Category 1"
+image_folder = ORIGINAL_FOLDER+CATEGORY_1
+train_folder = TRAIN_DESTINATION_FOLDER + CATEGORY_1
+test_folder = TEST_DESTINATION_FOLDER
+copy_training_and_testing_sets(image_folder,train_folder,test_folder)
+
+print "\n Getting training and test for Category 2"
+image_folder = ORIGINAL_FOLDER+CATEGORY_2
+train_folder = TRAIN_DESTINATION_FOLDER + CATEGORY_2
+test_folder = TEST_DESTINATION_FOLDER
+copy_training_and_testing_sets(image_folder,train_folder,test_folder)
