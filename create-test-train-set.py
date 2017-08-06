d062f30147a86a13a917b9c4860d6b3ad611a795
Jean-Frederic Plante <jfrederic.plante@gmail.com>
2017-08-06 00:17:33 -0700

    working training and testing split script

--- create-test-train-set.py
+++ create-test-train-set.py
@@ -6,7 +6,7 @@ from math import floor
 
 
 # Parameters
-TRAINING_PERCENTAGE = 80
+TRAINING_PERCENTAGE = 80.0
 ORIGINAL_FOLDER = "processed"
 CATEGORY_1 = "/one"
 CATEGORY_2 = "/ten"
@@ -22,22 +22,26 @@ def get_file_list_from_dir(datadir):
 
 def get_training_and_testing_sets(file_list):
     split = TRAINING_PERCENTAGE/100
-    split_index = floor(len(file_list) * split)
+    split_index = int(floor(len(file_list) * split))
+    print "..Total size: ", len(file_list)
+    print "...Training size: ", split_index
+    print "...Testing size: ", len(file_list)-split_index
+
     training = file_list[:split_index]
     testing = file_list[split_index:]
     return training, testing
 
 def copy_training_and_testing_sets(image_folder, train_folder, test_folder):
     image_files = get_file_list_from_dir(image_folder)
-    print image_folder, image_files
+    shuffle(image_files)
     training_files, testing_files = get_training_and_testing_sets(image_files)
     for original_file in training_files:
-        shutil.copy(original_file,train_folder)
+        shutil.copy(image_folder + '/' + original_file, train_folder)
     for original_file in testing_files:
-        shutil.copy(original_file, test_folder)
+        shutil.copy(image_folder + '/' + original_file, test_folder)
 
 
-print "\n creating destination folders"
+print "\nCreating destination folders"
 shutil.rmtree(TRAIN_DESTINATION_FOLDER, ignore_errors = True)
 shutil.rmtree(TEST_DESTINATION_FOLDER, ignore_errors = True)
 
@@ -45,13 +49,13 @@ os.makedirs(TRAIN_DESTINATION_FOLDER+CATEGORY_1)
 os.makedirs(TRAIN_DESTINATION_FOLDER+CATEGORY_2)
 os.makedirs(TEST_DESTINATION_FOLDER)
 
-print "\n Getting training and test for Category 1"
+print "\nGetting training and test for Category 1"
 image_folder = ORIGINAL_FOLDER+CATEGORY_1
 train_folder = TRAIN_DESTINATION_FOLDER + CATEGORY_1
 test_folder = TEST_DESTINATION_FOLDER
 copy_training_and_testing_sets(image_folder,train_folder,test_folder)
 
-print "\n Getting training and test for Category 2"
+print "\nGetting training and test for Category 2"
 image_folder = ORIGINAL_FOLDER+CATEGORY_2
 train_folder = TRAIN_DESTINATION_FOLDER + CATEGORY_2
 test_folder = TEST_DESTINATION_FOLDER
