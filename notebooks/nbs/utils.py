aaa1031724e6dfd819864952ee9c42c95e9b8c0a
Jean-Frederic Plante <jfrederic.plante@gmail.com>
2017-08-07 23:17:30 -0700

    running first training, reverting to cPickle now that i'm on Python 2.

--- notebooks/nbs/utils.py
+++ notebooks/nbs/utils.py
@@ -1,6 +1,6 @@
 from __future__ import division,print_function
 import math, os, json, sys, re
-import pickle
+import cPickle as pickle
 from glob import glob
 import numpy as np
 from matplotlib import pyplot as plt
